f3851b615302c0aeb5de0b9c0795a5040b070373
rebeccajohnson88 <bjohnson88@gmail.com>
2023-04-28 14:59:43 -0400

    updates

---
+++ final/ml/model.py
@@ -0,0 +1,70 @@
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+from sklearn.linear_model import LogisticRegression
+
+
+# Optional: implement hyperparameter tuning.
+def train_model(X_train, y_train):
+    """
+    Trains a machine learning model and returns it.
+
+    Inputs
+    ------
+    X_train : np.array
+        Training data.
+    y_train : np.array
+        Labels.
+    Returns
+    -------
+    model
+        Trained machine learning model.
+    """
+
+    # penalized logistic regression
+    lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
+
+    # fit model
+    lrc.fit(X_train, y_train)
+    
+    return lrc
+
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    return precision, recall, fbeta
+
+
+def inference(model, X):
+    """ Run model inferences and return the predictions.
+
+    Inputs
+    ------
+    model : penalized logistic regression
+    X : np.array
+        Data used for prediction.
+    Returns
+    -------
+    preds : np.array
+        Predictions from the model.
+    """
+    preds = model.predict(X)
+    return preds 
+
+
